e5d3092ae85d0522fec91009b01892f0bbba18f9
Maksim Kochukov <kocetkovm968@gmail.com>
2026-03-16 14:33:56 +0300

    Выполнил задание 1 урока

---
+++ script.js
@@ -0,0 +1,10 @@
+let title = "Заголовок";
+let screens = "Экраны";
+let screenPrice = 150;
+let rollback = "Вернуть назад";
+let fullPrice = 1000;
+let adaptive = "Адаптивный";
+
+console.log(screenPrice);
+
+alert("Полная цена услуги: " + fullPrice);
